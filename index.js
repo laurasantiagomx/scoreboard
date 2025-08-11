a4267aafe694f1b2589b05956d94a5379fbcc46c
laurasantiagomx <laurasantiagomx@gmail.com>
2025-08-11 14:59:59 -0600

    console(result) changed

--- index.js
+++ index.js
@@ -6,37 +6,37 @@ let num2 = 5
 
 function home1() {
     let result = num1 += 1
-    console.log(1)
     homeScore.textContent = result
+    console.log(result)
 }
 
 function home2() {
     let result = num1 += 2
-    console.log(2)
     homeScore.textContent = result
+    console.log(result)
 }
 
 function home3() {
     let result = num1 += 3 
-    console.log(3)
     homeScore.textContent = result
+    console.log(result)
 }
 
 function guest1() {
     let result = num2 += 1
-    console.log(1) 
     guestScore.textContent = result
+    console.log(result)
 }
 
 function guest2() {
     let result = num2 += 2
-    console.log(2)
     guestScore.textContent = result
+    console.log(result)
 }
 
 function guest3() {
     let result = num2 += 3
-    console.log(3)
     guestScore.textContent = result
+    console.log(result)
 }
 
